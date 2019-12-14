70437daba15357c53e76ebe506512d952bab88a8
Stefan Särne <ssa@spotify.com>
2019-12-14 13:44:08 +0100

    Dec 11

---
+++ src/main/java/aoc/aoc2019/Dec11.java
@@ -0,0 +1,385 @@
+package aoc.aoc2019;
+
+import static aoc.Utils.getLines;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
+
+public class Dec11 {
+
+  public static void main(String[] args) throws Exception {
+    task1();
+    task2();
+  }
+
+  public static void task1() throws Exception {
+    HashMap<String, Long> panels = drawPanels(0L);
+    System.out.println("Result: " + panels.size());
+  }
+
+  public static void task2() throws Exception {
+    HashMap<String, Long> panels = drawPanels(1L);
+    System.out.println("Result: " + panels.size());
+    printPanels(panels);
+  }
+
+  private static HashMap<String, Long> drawPanels(long initial) {
+
+    HashMap<String, Long> panels = new HashMap<>();
+    panels.put("0,0", initial);
+    int x = 0, y = 0;
+    char dir = '^';
+    long color, turn;
+
+    Program p = Program.create(getLines().get(0));
+    while (p.status != Program.Status.HALT_OK) {
+      color = color(panels, pos(x, y));
+      p.input.add(color);
+
+      IntCode.execute(p);
+      color = p.output.poll();
+      turn = p.output.poll();
+      panels.put(pos(x, y), color);
+      dir = getDir(dir, turn);
+      x += dx(dir);
+      y += dy(dir);
+      // System.out.println("Step: " + dir + " " + x + "," + y + " " + newColor);
+    }
+    return panels;
+  }
+
+  private static long color(HashMap<String, Long> panels, String pos) {
+    return panels.containsKey(pos) ? panels.get(pos) : 0L;
+  }
+
+  private static int dx(char dir) {
+    if (dir == '<') {
+      return -1;
+    }
+    if (dir == '>') {
+      return 1;
+    }
+    return 0;
+  }
+
+  private static int dy(char dir) {
+    if (dir == '^') {
+      return 1;
+    }
+    if (dir == 'v') {
+      return - 1;
+    }
+    return 0;
+  }
+
+  private static String pos(int x, int y) {
+    return x + "," + y;
+  }
+
+  public static void printPanels(HashMap<String, Long> panels) {
+    int minx = 0, maxx = 0, miny = 0, maxy = 0;
+    for (String panel : panels.keySet()) {
+      minx = Math.min(minx, Integer.parseInt(panel.split(",")[0]));
+      maxx = Math.max(maxx, Integer.parseInt(panel.split(",")[0]));
+      miny = Math.min(miny, Integer.parseInt(panel.split(",")[1]));
+      maxy = Math.max(maxy, Integer.parseInt(panel.split(",")[1]));
+    }
+    for (int y = maxy; y >= miny; y--) {
+      for (int x = maxx; x >= minx; x--) {
+        System.out.print(color(panels, pos(x, y)) == 1 ? "*" : " ");
+      }
+      System.out.println();
+    }
+  }
+
+  private static char getDir(char dir, long turn) {
+    switch (dir) {
+      case '^':
+        return turn == 1L ? '<' : '>';
+      case '>':
+        return turn == 1L ? '^' : 'v';
+      case '<':
+        return turn == 1L ? 'v' : '^';
+      case 'v':
+        return turn == 1L ? '>' : '<';
+      default:
+        throw new RuntimeException("CMH");
+    }
+  }
+
+  private static class Program {
+
+    enum Status {
+      HALT_OK,
+      BLOCKED,
+      ERROR,
+      NA
+    }
+
+    Queue<Long> input;
+    Queue<Long> output;
+    Program.Status status;
+    long pc;
+    Memory mem;
+    long offset = 0;
+
+    Program(long[] memory) {
+      this.pc = 0;
+      this.mem = new Memory(memory);
+      this.status = Program.Status.NA;
+      this.input = new ConcurrentLinkedQueue();
+      this.output = new ConcurrentLinkedQueue();
+    }
+
+    void print() {
+      System.out.println("======================= pc=" + pc + "   input=" + input.toString());
+      System.out.println("======================= memory=" + mem.toString());
+    }
+
+    public String getOutputAsString() {
+      return output.stream().map(n -> String.valueOf(n)).collect(Collectors.joining(","));
+    }
+
+    static Program create(long[] memory) {
+      return new Program(memory);
+    }
+
+    static Program create(long[] memory, long[] input) {
+      Program p = new Program(memory);
+      for (long i : input) {
+        p.input.add(i);
+      }
+      return p;
+    }
+
+    static Program create(String program) {
+      long[] memory = Arrays.stream(program.split(",")).mapToLong(Long::parseLong).toArray();
+      return create(memory);
+    }
+  }
+
+  private static class Memory {
+
+    HashMap<Long, Long> map = new HashMap<>();
+
+    public Memory(long[] memory) {
+      for (int i = 0; i < memory.length; i++) {
+        store(i, memory[i]);
+      }
+    }
+
+    long read(long addr) {
+      if (!map.containsKey(addr)) {
+        map.put(addr, 0L);
+      }
+      Long chunk = map.get(addr);
+      return chunk.longValue();
+    }
+
+    public void store(long addr, long value) {
+      map.put(addr, value);
+    }
+  }
+
+  private static class IntCode {
+
+    static Program execute(Program p) {
+
+      // p.print();
+
+      while (true) {
+
+        Memory mem = p.mem;
+        final long pc = p.pc;
+        final long offset = p.offset;
+
+        int op = (int) read(pc, mem);
+        long p1 = 0, p2 = 0, p3 = 0, v1 = 0, v2 = 0, v3 = 0;
+        int m1 = 0, m2 = 0, m3 = 0;
+
+        if (op >= 100) {
+          int modes = op / 100;
+          m1 = (modes / 1) % 10;
+          m2 = (modes / 10) % 10;
+          m3 = (modes / 100) % 10;
+          op = op % 100;
+        }
+
+        // printOp(pc, mem);
+
+        switch (op) {
+          // Opcode 1 adds together numbers read from two positions and stores the result in a third position.
+          case 1: // ADD
+            p1 = read(pc + 1, mem);
+            p2 = read(pc + 2, mem);
+            p3 = read(pc + 3, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            v2 = resolve(p2, m2, mem, offset);
+            v3 = resolve(p3, m3, mem, offset, true);
+            if (m3 == 1) {
+              throw new RuntimeException(("CMH"));
+            }
+            store(v3, v1 + v2, mem);
+            p.pc += 4;
+            break;
+
+          // Opcode 2 multiplies the two inputs and stores the result in a third position.
+          case 2: // MUL
+            p1 = read(pc + 1, mem);
+            p2 = read(pc + 2, mem);
+            p3 = read(pc + 3, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            v2 = resolve(p2, m2, mem, offset);
+            v3 = resolve(p3, m3, mem, offset, true);
+            if (m3 == 1) {
+              throw new RuntimeException(("CMH"));
+            }
+            store(v3, v1 * v2, mem);
+            p.pc += 4;
+            break;
+
+          // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter
+          case 3: // INPUT
+            if (p.input.isEmpty()) {
+              p.status = Program.Status.BLOCKED;
+              return p;
+            }
+            p1 = read(pc + 1, mem);
+            v1 = resolve(p1, m1, mem, offset, true);
+            v2 = p.input.poll();
+            store(v1, v2, mem);
+            p.pc += 2;
+            break;
+
+          // Opcode 4 outputs the value of its only parameter.
+          case 4: // OUTPUT
+            p1 = read(pc + 1, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            p.output.add(v1);
+            p.pc += 2;
+            break;
+
+          // Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction
+          // pointer to the value from the second parameter. Otherwise, it does nothing.
+          case 5:
+            p1 = read(pc + 1, mem);
+            p2 = read(pc + 2, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            v2 = resolve(p2, m2, mem, offset);
+            p.pc = (v1 != 0 ? v2 : pc + 3);
+            break;
+
+          // Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction
+          // pointer to the value from the second parameter. Otherwise, it does nothing.
+          case 6:
+            p1 = read(pc + 1, mem);
+            p2 = read(pc + 2, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            v2 = resolve(p2, m2, mem, offset);
+            p.pc = (v1 == 0 ? v2 : pc + 3);
+            break;
+
+          // Opcode 7 is less than: if the first parameter is less than the second parameter, it
+          // stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+          case 7:
+            p1 = read(pc + 1, mem);
+            p2 = read(pc + 2, mem);
+            p3 = read(pc + 3, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            v2 = resolve(p2, m2, mem, offset);
+            v3 = resolve(p3, m3, mem, offset, true);
+            if (m3 == 1) {
+              throw new RuntimeException(("CMH"));
+            }
+            store(v3, (v1 < v2 ? 1 : 0), mem);
+            p.pc += 4;
+            break;
+
+          // Opcode 8 is equals: if the first parameter is equal to the second parameter, it
+          // stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+          case 8:
+            p1 = read(pc + 1, mem);
+            p2 = read(pc + 2, mem);
+            p3 = read(pc + 3, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            v2 = resolve(p2, m2, mem, offset);
+            v3 = resolve(p3, m3, mem, offset, true);
+            if (m3 == 1) {
+              throw new RuntimeException(("CMH"));
+            }
+            store(v3, (v1 == v2 ? 1 : 0), mem);
+            p.pc += 4;
+            break;
+
+          // Opcode 9 adjusts the relative base by the value of its only parameter.
+          case 9:
+            p1 = read(pc + 1, mem);
+            v1 = resolve(p1, m1, mem, offset);
+            p.offset += v1;
+            p.pc += 2;
+            break;
+
+          case 99:
+            p.status = Program.Status.HALT_OK;
+            return p;
+
+          default:
+            System.out.print("Error, pc=" + pc + " op=" + op);
+            p.status = Program.Status.ERROR;
+            return p;
+        }
+      }
+    }
+
+    private static void store(long addr, long value, Memory mem) {
+      // mem[addr] = value;
+      mem.store(addr, value);
+    }
+
+    private static long read(long addr, Memory mem) {
+      // return mem[(int) addr];
+      return mem.read(addr);
+    }
+
+    private static long resolve(long p, int m, Memory mem, long offset) {
+      return resolve(p, m, mem, offset, false);
+    }
+
+    private static long resolve(long p, int m, Memory mem, long offset, boolean write) {
+      // return (m1 == 0 ? mem[(int) p1] : p1);
+      switch (m) {
+        case 0:
+          if (write) {
+            return p;
+          }
+          return mem.read(p);
+        case 1:
+          if (write) {
+            throw new RuntimeException("CMH");
+          }
+          return p;
+        case 2:
+          if (write) {
+            return p + offset;
+          }
+          return mem.read(p + offset);
+        default:
+          throw new RuntimeException("CMH");
+      }
+    }
+
+    static void printOp(long pc, Memory mem) {
+      System.out.printf(
+          "[%3d] %4s %4s %4s %4s   ||",
+          pc, mem.read(pc), mem.read(pc + 1), mem.read(pc + 2), mem.read(pc + 3));
+
+      for (int i = 0; i < 48; i++) {
+        System.out.printf("  %s%4s", mem.read(i), (i % 4 == 3 ? " _ " : ""));
+      }
+      System.out.println();
+    }
+  }
+}
