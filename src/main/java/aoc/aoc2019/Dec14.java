22d28c035f181a2c846580a034132f633a4e1202
Stefan Särne <ssa@spotify.com>
2019-12-16 20:40:56 +0100

    wip

---
+++ src/main/java/aoc/aoc2019/Dec14.java
@@ -0,0 +1,56 @@
+package aoc;
+
+import static aoc.Utils.*;
+import static java.lang.System.out;
+
+import java.util.*;
+
+public class AdventOfCodeTemplate {
+
+  public static void main(String[] args) throws Exception {
+    test();
+    // task1();
+    // task2();
+  }
+
+  public static void test() throws Exception {
+    String result = doit("FIXME");  // aoc2019/decXX.txt"
+    check(result, "FIXME");
+  }
+
+  public static void task1() throws Exception {
+    String result = doit("FIXME");
+    System.out.println("Result: " + result);
+  }
+
+  public static void task2() throws Exception {
+    String result = doit("FIXME");
+    System.out.println("Result: " + result);
+  }
+
+  public static String doit(String input) throws Exception {
+
+    List<String> lines = getLines(input);
+
+    for (String line : lines) {
+      // print(recipes, lenght, x, y);
+    }
+
+    String result = "FIXME";
+    return result;
+  }
+
+  private static int calc(int i) {
+    int result = 0;
+    return result;
+  }
+
+  private static int calc2(int i) {
+    int result = 0;
+    return result;
+  }
+
+  public static void print() {
+    out.println("");
+  }
+}
