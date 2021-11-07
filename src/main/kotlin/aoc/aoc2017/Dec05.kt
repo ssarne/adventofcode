7335f41eec14ba00954c8bc02133af72d4dae4c9
ssarne <stefan.sarne@gmail.com>
2021-11-07 22:24:36 +0100

    Dec 05 - 2017

---
+++ src/main/kotlin/aoc/aoc2017/Dec05.kt
@@ -0,0 +1,46 @@
+package aoc.aoc2017
+
+import aoc.ktutils.check
+import aoc.utils.Utils
+
+fun main() {
+    check(countJumps(input("aoc2017/dec05_test.txt")), 5)
+    println(countJumps(input(null))) // 394829
+    check(countJumps2(input("aoc2017/dec05_test.txt")), 10)
+    println(countJumps2(input(null))) // 31150702
+}
+
+fun countJumps(instructions: IntArray): Int {
+    var steps = 0
+    var pos = 0
+
+    while (pos >= 0 && pos < instructions.size) {
+        var offset = instructions[pos]
+        instructions[pos]++
+        pos += offset
+        steps++
+    }
+
+    return steps
+}
+
+fun countJumps2(instructions: IntArray): Int {
+    var steps = 0
+    var pos = 0
+
+    while (pos >= 0 && pos < instructions.size) {
+        var offset = instructions[pos]
+        if (offset < 3) instructions[pos]++ else instructions[pos]--
+        pos += offset
+        steps++
+    }
+
+    return steps
+}
+
+private fun input(fileName: String?): IntArray {
+    var instr = Utils.getLines(fileName)
+        .map { line -> line.toInt() }
+        .toIntArray()
+    return instr
+}
