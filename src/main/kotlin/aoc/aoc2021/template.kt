559e997306f0e234fddbd7ecd41fe46d9da900df
ssarne <stefan.sarne@gmail.com>
2021-12-10 15:17:06 +0100

    Kotlin template

---
+++ src/main/kotlin/aoc/aoc2021/template.kt
@@ -0,0 +1,20 @@
+package aoc.aoc2021
+
+import aoc.ktutils.*
+import kotlin.math.*
+import java.lang.RuntimeException
+
+fun main() {
+    check(execute1(readTestLines()), 0)
+    println(execute1(readLines())) //
+    check(execute2(readTestLines()), 0)
+    println(execute2(readLines())) //
+}
+
+private fun execute1(input: List<String>): Int {
+    return -1
+}
+
+private fun execute2(input: List<String>): Int {
+    return -1
+}
