57d35fe308274f21e45dfc3fc06cb222334a3038
ssarne <stefan.sarne@gmail.com>
2022-12-11 16:47:42 +0100

    Move template to top level, use input/ folder and .out for answers.

---
+++ src/main/kotlin/aoc/template.kt
@@ -0,0 +1,21 @@
+package aoc
+
+import aoc.ktutils.*
+import kotlin.math.*
+import java.lang.RuntimeException
+
+fun main() {
+    check(execute1(readTestLines()), "fixme")
+    execute1(readLines()).let { println(it); check(it, readAnswer(1)) }
+
+    //check(execute2(readTestLines()), "fixme")
+    //execute2(readLines()).let { println(it); check(it, readAnswer(2)) }
+}
+
+private fun execute1(input: List<String>): String {
+    return "na"
+}
+
+private fun execute2(input: List<String>): String {
+    return "na"
+}
