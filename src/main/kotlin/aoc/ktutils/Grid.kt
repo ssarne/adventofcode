788bf6e5505f9d9e20d8e51cc3ec22f11a3bc3b1
ssarne <stefan.sarne@gmail.com>
2023-01-02 10:08:38 +0100

    Add print utility for reversed grids.

--- src/main/kotlin/aoc/ktutils/Grid.kt
+++ src/main/kotlin/aoc/ktutils/Grid.kt
@@ -1,6 +1,8 @@
 package aoc.ktutils
 
-import java.lang.RuntimeException
+import java.util.BitSet
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 fun parseSparseBinaryMatrix(input: List<String>): HashSet<Point> {
     if (input.isEmpty()) throw RuntimeException("Height should be larger than 0. Input list is empty.")
@@ -27,12 +29,12 @@ fun parseCharacterGridToMap(input: List<String>): HashMap<Point, Char> {
     return grid
 }
 
-fun printSparseMatrix(dots: Map<Point, Char>, header: Boolean = false) {
+fun printSparseMatrix(dots: Map<Point, Char>, header: Boolean = false, msg: String = "") {
     val minX = dots.keys.stream().mapToInt { it.x }.min().orElse(0)
     val minY = dots.keys.stream().mapToInt { it.y }.min().orElse(0)
     val maxX = dots.keys.stream().mapToInt { it.x }.max().orElse(1)
     val maxY = dots.keys.stream().mapToInt { it.y }.max().orElse(1)
-    if (header) for (x in minX..maxX) print("-"); println("")
+    if (header) for (x in minX..maxX) print("-"); println("  $msg")
     for (y in minY..maxY) {
         for (x in minX..maxX) {
             if (dots.contains(Point(x, y))) {
@@ -56,6 +58,25 @@ fun printSparseMatrixReversed(dots: Map<Point, Char>, header: Boolean = false) {
         for (x in minX..maxX) {
             if (dots.contains(Point(x, y))) {
                 print(dots[Point(x, y)])
+            } else {
+                print(' ')
+            }
+        }
+        println()
+    }
+}
+
+fun printBitSetMatrixReversed(dots: BitSet, width: Int, header: Boolean = false) {
+    val minX = 0
+    val minY = 0
+    val maxX = width
+    val maxY = 1 + dots.length() / width
+    if (header) for (x in minX..maxX) print("-"); println("")
+    for (y1 in minY..maxY) {
+        var y = maxY - y1
+        for (x in minX until maxX) {
+            if (dots[y * width + x]) {
+                print('#')
             } else {
                 print(' ')
             }
