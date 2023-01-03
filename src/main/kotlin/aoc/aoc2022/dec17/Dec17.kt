e503310b8d0800d8f7ce511737333ad530ced20c
ssarne <stefan.sarne@gmail.com>
2023-01-03 17:50:44 +0100

    Dec 17 - 2022 - part 2
    Continue simulate the rocks falling
    Calculate heights for each column (from the lowest position of any of the top values)
    Store position in rocks, position in jet stream and the heights
    On match, a complete cycle is found
    
    (actually the height state is a simplification, but it is good enough)

--- src/main/kotlin/aoc/aoc2022/dec17/Dec17.kt
+++ src/main/kotlin/aoc/aoc2022/dec17/Dec17.kt
@@ -1,87 +1,168 @@
 package aoc.aoc2022.dec17
 
 import aoc.ktutils.*
+import java.util.*
 import kotlin.math.*
-import kotlin.RuntimeException
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 fun main() {
-    check(execute1(readTestLines(), 5), 9)
-    check(execute1(readTestLines(), 10), 17)
-    check(execute1(readTestLines(), 2022), 3068)
-    execute1(readLines(), 2022).let { println(it) } // ; check(it, readAnswerAsInt(1)) }
+    check(execute(readTestLines(), 5), 9)
+    check(execute(readTestLines(), 10), 17)
+    check(execute(readTestLines(), 2022), 3068)
+    execute(readLines(), 2022).let { println(it); check(it, readAnswerAsLong(1)) }
 
-    // check(execute2(readTestLines()), -1)
-    // execute2(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(2)) }
+    check(execute(readTestLines(), 1000000000000L), 1514285714288L)
+    execute(readLines(), 1000000000000L).let { println(it); check(it, readAnswerAsLong(2)) }
 }
 
-private fun execute1(input: List<String>, iterations: Int): Int {
-    val (rocks, winds) = parse(input)
-    val cave = HashMap<Point, Char>()
+// wind index, rock index, heights -> iteration, top
+private data class MemKey(val jet: Int, val rock: Int, val heights: ArrayList<Int>)
+private data class MemValue(val iteration: Long, val top: Long)
 
-    var floor = 0
-    val width = 7
-    val entryX = 3
-    for (x in 1..width) cave[Point(x, floor)] = '-'
-    var top = floor
-    var windIterator = 0
+private fun execute(input: List<String>, iterations: Long): Long {
 
-    for (r in 0 until iterations) {
-        val rock = rocks[r % rocks.size]
+    val (rocks, jets) = parse(input)
+    var cave = BitSet()
+    val debug = false
+
+    val memory = HashMap<MemKey, MemValue>() // Memoization
+
+    var iter = 0L    // number of rocks dropped
+    var rocki = -1   // index in the rocks list
+    var jeti = -1    // index in the jet stream
+    var top = 0      // height of top line for the rocks
+    var offset = 0L  // offset of cave, and actual top
+    val width = 7    // width of cave
+    val entryX = 2   // x coordinate for rocks to enter
+
+    for (x in 0 until width) cave[0 * 7 + x] = true
+
+    while (iter < iterations) {
+
+        if (debug) {
+            println("=== $top ===")
+            printBitSetMatrixReversed(cave, width)
+            println(heights(cave, width))
+            println("=== === ===")
+        }
+
+        iter++
+        rocki = (rocki + 1) % rocks.size
+
+        val rock = rocks[rocki]
         var pos = Point(entryX, top + 3 + 1)
+        var falling = true
 
-        while (true) {
-            //addRock(cave, rock, pos, '@')
-            //printSparseMatrixReversed(cave)
-            //removeRock(cave, rock, pos)
+        while (falling) {
+
+            if (debug) {
+                println("=== $top ===")
+                addRock(cave, width, rock, pos)
+                printBitSetMatrixReversed(cave, width)
+                removeRock(cave, width, rock, pos)
+                println("=== === ===")
+            }
 
             // blow in wind
-            val d = winds[windIterator++ % winds.length]
-            if (d == '<' && pos.x > 1 && space(cave, rock, Point(pos.x - 1, pos.y)))
-                pos = Point(pos.x - 1, pos.y)
-            else if (d == '>' && pos.x + rock[0].length <= width && space(cave, rock, Point(pos.x + 1, pos.y)))
-                pos = Point(pos.x + 1, pos.y)
+            jeti = (jeti + 1) % jets.length
+            when (jets[jeti]) {
+                '<' -> if (pos.x > 0)
+                    Point(pos.x - 1, pos.y).let {
+                        if (space(cave, width, rock, it))
+                            pos = it
+                    }
+
+                '>' -> if (pos.x + rock[0].length < width)
+                    Point(pos.x + 1, pos.y).let {
+                        if (space(cave, width, rock, it))
+                            pos = it
+                    }
+            }
 
             // check if down - break
-            if (space(cave, rock, Point(pos.x, pos.y - 1))) {
-                pos = Point(pos.x, pos.y - 1)
-            } else {
-                addRock(cave, rock, pos, '#')
-                top = max(pos.y + rock.size - 1, top)
+            Point(pos.x, pos.y - 1).let {
+                if (space(cave, width, rock, it)) {
+                    pos = it
+                } else {
+                    addRock(cave, width, rock, pos)
+                    top = max(pos.y + rock.size - 1, top)
+                    falling = false
+                }
+            }
+        }
+
+        // Memoize
+        val heights = heights(cave, width)
+        val key = MemKey(jeti, rocki, heights)
+        if (memory.containsKey(key)) { // found match, skip ahead as far as possible
+            val value = memory[key]!!
+            val dCount = iter - value.iteration
+            val dTop = offset + top.toLong() - value.top
+            if (debug) println("Match for $key :: ${value.iteration}->$iter dCount=$dCount dTop=$dTop")
+            val jumps = (iterations - iter) / dCount
+            iter += jumps * dCount
+            offset += jumps * dTop
+        } else {
+            memory[key] = MemValue(iter, offset + top)
+        }
+    }
+    return offset + top
+}
+
+private fun heights(cave: BitSet, width: Int): ArrayList<Int> {
+
+    val heights = ArrayList<Int>(width)
+    for (x in 0 until width) heights.add(0)
+    val above = (cave.size() / width)
+    var low = above
+    for (x in 0 until width) {
+        for (i in 0..above) {
+            val h = above - i
+            val b = h * width + x
+            if (cave[b]) {
+                heights[x] = h
+                low = min(h, low)
                 break
             }
         }
-        //print("=== $top ===")
-        //printSparseMatrixReversed(cave)
     }
-    return top
+
+    // Normalize to lowest top
+    for (i in 0 until width) {
+        heights[i] = heights[i] - low
+    }
+
+    return heights
 }
 
-fun addRock(cave: HashMap<Point, Char>, rock: ArrayList<String>, pos: Point, c: Char) {
+fun addRock(cave: BitSet, width: Int, rock: ArrayList<String>, pos: Point) {
+    for (y in rock.indices) {
+        for (x in rock[y].indices) {
+            if (rock[y][x] == '#') {
+                val xx = pos.x + x
+                val yy = pos.y + y
+                cave[yy * width + xx] = true
+            }
+        }
+    }
+}
+
+fun removeRock(cave: BitSet, width: Int, rock: ArrayList<String>, pos: Point) {
     for (y in rock.indices)
         for (x in rock[y].indices)
             if (rock[y][x] == '#')
-                cave[Point(pos.x + x, pos.y + y)] = c
+                cave[(pos.y + y) * width + (pos.x + x)] = false
 }
 
-fun removeRock(cave: HashMap<Point, Char>, rock: ArrayList<String>, pos: Point) {
+fun space(cave: BitSet, width: Int, rock: ArrayList<String>, pos: Point): Boolean {
+    if (pos.y < 0) return false
     for (y in rock.indices)
         for (x in rock[y].indices)
             if (rock[y][x] == '#')
-                cave.remove(Point(pos.x + x, pos.y + y))
-}
-
-fun space(cave: HashMap<Point, Char>, rock: ArrayList<String>, pos: Point): Boolean {
-    for (y in rock.indices)
-        for (x in rock[y].indices)
-            if (rock[y][x] == '#')
-                if (cave.containsKey(Point(pos.x + x, pos.y + y)))
+                if (cave[(pos.y + y) * width + (pos.x + x)])
                     return false
     return true
-
-}
-
-private fun execute2(input: List<String>): Int {
-    return 0
 }
 
 private fun parse(input: List<String>): Pair<ArrayList<ArrayList<String>>, String> {
