a180e29b27238708f60150a97da53db0ebc3000a
ssarne <stefan.sarne@gmail.com>
2023-01-04 22:11:29 +0100

    Dec 22 - 2022 - Part 2
    Walk the path using a wrapCube function for non-trivial side switches
    Identify cube layout, then which side of it is in use, and what direction
    Calculate mapping based on each side, hardcoded based on layout
    (will only work for the cube in my input, and test cube)

--- src/main/kotlin/aoc/aoc2022/dec22/Dec22.kt
+++ src/main/kotlin/aoc/aoc2022/dec22/Dec22.kt
@@ -1,19 +1,21 @@
 package aoc.aoc2022.dec22
 
-
 import aoc.ktutils.*
 import java.lang.Math.floorMod
 import kotlin.math.*
 import kotlin.RuntimeException
 
 fun main() {
-    // check(execute1(readTestLines()), 6032)
-    //execute1(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(1)) }
+    check(execute1(readTestLines()), 6032)
+    execute1(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(1)) }
 
-    testWrap2()
+    testWrapCube()
     check(execute2(readTestLines()), 5031)
-    //execute2(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(2)) }
+    execute2(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(2)) }
+    // not 62256
 }
+
+private data class Pos(val p: Point, val d: Char)
 
 private fun execute1(input: List<String>): Int {
 
@@ -50,7 +52,7 @@ private fun execute1(input: List<String>): Int {
                         else -> throw RuntimeException("CMH")
                     }
                     if (!map.containsKey(next.p)) {
-                        next = Pos(wrap1(pos, map, width, height), pos.d)
+                        next = Pos(wrapFlat(pos, map, width, height), pos.d)
                     }
                     if (map[next.p] == '#') break
                     pos = next
@@ -67,9 +69,7 @@ private fun execute1(input: List<String>): Int {
 private fun execute2(input: List<String>): Int {
 
     val (map, path) = parse(input)
-    val width = map.keys.map { it.x }.max()!!
-    val height = map.keys.map { it.y }.max()!!
-    val side = max(width, height) / 4
+    val (size, layout) = identify(map)
     //printSparseMatrix(map)
 
     val dirs = charArrayOf('>', 'v', '<', '^')
@@ -99,12 +99,16 @@ private fun execute2(input: List<String>): Int {
                         '^' -> Pos(Point(pos.p.x, pos.p.y - 1), '^')
                         else -> throw RuntimeException("CMH")
                     }
+
                     if (!map.containsKey(next.p)) {
-                        next = wrap2(pos, side, map)
+                        // printSparseMatrix(map, true, "t=$t  pos=$pos")
+                        next = wrapCube(pos, size, layout)
                     }
+
                     if (map[next.p] == '#')
                         break
-                    // if (map[next] == '.') pos = next
+
+                    pos = next
                     map[pos.p] = pos.d
                 }
             }
@@ -115,11 +119,33 @@ private fun execute2(input: List<String>): Int {
     return 1000 * pos.p.y + 4 * pos.p.x + dirs.indexOf(pos.d)
 }
 
-private data class Pos(val p: Point, val d: Char)
+fun identify(map: HashMap<Point, Char>): Pair<Int, String> {
 
-private fun turnRight(dirs: CharArray, dir: Char) = dirs[Math.floorMod(dirs.indexOf(dir) + 1, dirs.size)]
-private fun turnLeft(dirs: CharArray, dir: Char) = dirs[Math.floorMod(dirs.indexOf(dir) - 1, dirs.size)]
-private fun wrap1(pos: Pos, map: HashMap<Point, Char>, width: Int, height: Int): Point {
+    val width = map.keys.map { it.x }.max()!!
+    val height = map.keys.map { it.y }.max()!!
+    val size = max(width, height) / 4
+
+    var layout = ""
+    for (y in 0..3) {
+        for (x in 0..3) {
+            val p = Point(x * size + 1, y * size + 1)
+            if (map.containsKey(p))
+                layout += side(p, size)
+        }
+    }
+    return Pair(size, layout)
+}
+
+private fun side(p: Point, size: Int): Char {
+    val ids = "ABCD" + "EFGH" + "IJKL" + "MNOP"
+    val x = (p.x - 1) / size
+    val y = (p.y - 1) / size
+    return ids[4 * y + x]
+}
+
+private fun turnRight(dirs: CharArray, dir: Char) = dirs[floorMod(dirs.indexOf(dir) + 1, dirs.size)]
+private fun turnLeft(dirs: CharArray, dir: Char) = dirs[floorMod(dirs.indexOf(dir) - 1, dirs.size)]
+private fun wrapFlat(pos: Pos, map: HashMap<Point, Char>, width: Int, height: Int): Point {
     when (pos.d) {
         '>' -> for (x in 0..width)
             if (map.containsKey(Point(x, pos.p.y)))
@@ -140,86 +166,52 @@ private fun wrap1(pos: Pos, map: HashMap<Point, Char>, width: Int, height: Int):
     throw RuntimeException("CMH")
 }
 
-private fun testWrap2() {
-    val (map, path) = parse(readTestLines(2))
-    check(wrap2(Pos(Point(6, 1), '^'), 4, map), Pos(Point(1, 14), '>'))
-    check(wrap2(Pos(Point(12, 2), '>'), 4, map), Pos(Point(8, 11), '<'))
-    check(wrap2(Pos(Point(6, 12), 'v'), 4, map), Pos(Point(4, 14), '<'))
-    check(wrap2(Pos(Point(3, 9), '^'), 4, map), Pos(Point(5, 7), '>'))
-    check(wrap2(Pos(Point(8, 8), '>'), 4, map), Pos(Point(12, 4), '^'))
-}
+/**
+ * Wrap the edge of a cube and return the position on next side
+ * @param pos For current position
+ * @param size For size of a side of the cube
+ * @param layout For which sides in a A..D/E..F/G..J/K..N grid the cube map uses
+ */
+private fun wrapCube(pos: Pos, size: Int, layout: String): Pos {
 
-private fun check(actual: Pos, expected: Pos) {
-    if (actual != expected) {
-        System.err.println("Failure: actual=$actual  expected=$expected")
+    val mx = if (pos.p.x % size == 0) size else pos.p.x % size
+    val my = if (pos.p.y % size == 0) size else pos.p.y % size
+    val side = side(pos.p, size)
+    val label = "" + side + pos.d
+
+    if (layout == "BCFIJM") {
+        when (label) {
+            "C>" -> return Pos(Point(2 * size, 3 * size - my + 1), '<') // J<
+            "F>" -> return Pos(Point(2 * size + my, size), '^') // C^
+            "J>" -> return Pos(Point(3 * size, size - my + 1), '<') // C<
+            "M>" -> return Pos(Point(size + my, 3 * size), '^')  // J^
+            "B<" -> return Pos(Point(1, 3 * size - my + 1), '>')  // I>
+            "F<" -> return Pos(Point(my, 2 * size + 1), 'v')  // Iv
+            "I<" -> return Pos(Point(size + 1, size - my + 1), '>')  // B>
+            "M<" -> return Pos(Point(size + my, 1), 'v')  // Bv
+            "I^" -> return Pos(Point(size + 1, size + mx), '>')  // F>
+            "B^" -> return Pos(Point(1, 3 * size + mx), '>')  // M>
+            "C^" -> return Pos(Point(mx, 4 * size), '^')  // M^
+            "Mv" -> return Pos(Point(2 * size + mx, 1), 'v')  // Cv
+            "Jv" -> return Pos(Point(size, 3 * size + mx), '<')  // M<
+            "Cv" -> return Pos(Point(2 * size, size + mx), '<')  // F<
+            // "__" -> return Pos(Point(1, 1), '_')  // __
+            else -> throw RuntimeException("CMH: $layout ${pos.p} $label")
+        }
     }
-}
 
-
-
-private fun wrap2(
-    pos: Pos,
-    side: Int,
-    map: HashMap<Point, Char>): Pos {
-    val mx = if (pos.p.x % side == 0) side else pos.p.x % side
-    val my = if (pos.p.y % side == 0) side else pos.p.y % side
-    val full = 4 * side
-    when (pos.d) {
-        '>' -> {
-            val ps = mapOf(
-                Pair(Point(floorMod(pos.p.x + 1, full), floorMod(pos.p.y, full)), '>'),
-                Pair(Point(floorMod(pos.p.x + side - my + 1, full), floorMod(pos.p.y - my + side + 1, full)), 'v'),
-                Pair(Point(floorMod(pos.p.x + my, full), floorMod(pos.p.y - my, full)), '^'),
-                Pair(Point(floorMod(pos.p.x + side, full), floorMod(pos.p.y + 3 * side - my - my + 1, full)), '<'),
-                Pair(Point(floorMod(pos.p.x - side, full), floorMod(pos.p.y + 3 * side - my - my + 1, full)), '<')
-            )
-            for ((p, d) in ps)
-                if (map.containsKey(p))
-                    return Pos(p, d)
-        }
-        'v' -> {
-            val ps = mapOf(
-                Pair(Point(floorMod(pos.p.x, full), floorMod(pos.p.y + 1, full)), 'v'),
-                Pair(Point(floorMod(pos.p.x - mx, full), floorMod(pos.p.y + mx, full)), '<'),
-                Pair(Point(floorMod(pos.p.x - mx + side + 1, full), floorMod(pos.p.y + side - mx + 1, full)), '>'),
-                Pair(Point(floorMod(pos.p.x + 3 * side - mx - mx + 1, full), floorMod(pos.p.y + side, full)), '^'),
-                Pair(Point(floorMod(pos.p.x + 3 * side - mx - mx + 1, full), floorMod(pos.p.y - side, full)), '^'),
-            )
-            for ((p, d) in ps)
-                if (map.containsKey(p))
-                    return Pos(p, d)
-        }
-
-        '<' -> {
-            val ps = mapOf(
-                Pair(Point(floorMod(pos.p.x - 1, full), floorMod(pos.p.y, full)), '<'),
-                Pair(Point(floorMod(pos.p.x - my, full), floorMod(pos.p.y - my, full)), '^'),
-                Pair(Point(floorMod(pos.p.x - side + my - 1, full), floorMod(pos.p.y + side - my + 1, full)),'v' ),
-                Pair(Point(floorMod(pos.p.x - side, full), floorMod(pos.p.y - my - side - my + 1, full)),'>' ),
-                Pair(Point(floorMod(pos.p.x + side, full), floorMod(pos.p.y - my - side - my + 1, full)), '>'),
-            )
-            for ((p, d) in ps)
-                if (map.containsKey(p))
-                    return Pos(p, d)
-        }
-
-        '^' -> {
-            val ps = mapOf(
-                Pair(Point(floorMod(pos.p.x, full), floorMod(pos.p.y - 1, full)), '^'),
-                Pair(Point(floorMod(pos.p.x - mx, full), floorMod(pos.p.y - mx + 1, full)), '<'),
-                Pair(Point(floorMod(pos.p.x + side - mx + 1, full), floorMod(pos.p.y - side + mx - 1, full)), '>'),
-                Pair(Point(floorMod(pos.p.x - side - mx + 1, full), floorMod(pos.p.y - side + mx - 1, full)), '>'),
-                Pair(Point(floorMod(pos.p.x - mx - mx - side + 1, full), floorMod(pos.p.y + side, full)), 'v'),
-                Pair(Point(floorMod(pos.p.x - mx - mx - side + 1, full), floorMod(pos.p.y - side, full)), 'v'),
-            )
-            for ((p, d) in ps)
-                if (map.containsKey(p))
-                    return Pos(p, d)
+    if (layout == "CEFGKL") {
+        when (label) {
+            "G>" -> return Pos(Point(4 * size - my + 1, 2 * size + 1), 'v')  // Lv
+            "Kv" -> return Pos(Point(size - mx + 1, 2 * size), '^')  // E^
+            "F^" -> return Pos(Point(2 * size + 1, mx), '_')  // C>
+            // "__" -> return Pos(Point(1, 1), '_')  // __
+            else -> throw RuntimeException("CMH: $layout ${pos.p} $label")
         }
     }
 
     //printSparseMatrix(map)
-    throw RuntimeException("CMH $pos")
+    throw RuntimeException("CMH ${pos.p} $side ${pos.d}")
 }
 
 private fun parse(input: List<String>): Pair<HashMap<Point, Char>, List<String>> {
@@ -252,51 +244,60 @@ private fun parse(input: List<String>): Pair<HashMap<Point, Char>, List<String>>
     return Pair(grid, path)
 }
 
-/*
-when (dir) {
-    '>' -> {
-        if (pos.p.x == 3 * side && pos.p.y in 1..side)
-            return Pair(Point(4 * side, 3 * side - my + 1), '<')
-        if (pos.p.x == 3 * side && pos.p.y in side + 1..2 * side)
-            return Pair(Point(4 * side - my + 1, side * 2 + 1), 'v')
-        if (pos.p.x == 4 * side && pos.p.y in 2 * side + 1..3 * side)
-            return Pair(Point(3 * side, side - my + 1), '<')
-        throw RuntimeException("CMH $pos $dir")
-    }
+private fun testWrapCube() {
 
-    'v' -> {
-        if (pos.p.y == 2 * side && pos.p.x in 1..side)
-            return Pair(Point(3 * side - mx + 1, 3 * side), '^')
-        if (pos.p.y == 2 * side && pos.p.x in side + 1..2 * side)
-            return Pair(Point(2 * side + 1, 3 * side - mx + 1), '>')
-        if (pos.p.y == 3 * side && pos.p.x in 2 * side + 1..3 * side)
-            return Pair(Point(side - mx + 1, 2 * side), '^')
-        if (pos.p.y == 3 * side && pos.p.x in 3 * side + 1..4 * side)
-            return Pair(Point(1, 2 * side - mx + 1), '>')
-        throw RuntimeException("CMH $pos $dir")
-    }
-    '<' -> {
-        if (pos.p.x == 2 * side + 1 && pos.p.y in 1..side)
-            return Pair(Point(2 * side - my + 1, side + 1), 'v')
-        if (pos.p.x == 1 && pos.p.y in side + 1..2 * side)
-            return Pair(Point(4 * side - my + 1, 3 * side), '^')
-        if (pos.p.x == 2 * side + 1 && pos.p.y in 2 * side + 1..3 * side)
-            return Pair(Point(2 * side - my + 1, 2 * side), '^')
-        throw RuntimeException("CMH $pos $dir")
-    }
+    val (map0, _) = parse(readTestLines())
+    val (size0, layout0) = identify(map0)
+    check(size0, 4)
+    check(layout0, "CEFGKL")
+    checkWrapCube(Pos(Point(12, 6), '>'), size0, layout0, map0, Pos(Point(15, 9), 'v'))
 
-    '^' -> {
-        if (pos.p.y == 1 * side + 1 && pos.p.x in 1..side)
-            return Pair(Point(3 * side, side - mx + 1), '<')
-        if (pos.p.y == 1 * side + 1 && pos.p.x in side + 1..2 * side)
-            return Pair(Point(2 * side + 1, mx), '>')
-        if (pos.p.y == 1 && pos.p.x in 2 * side + 1..3 * side)
-            return Pair(Point(side - mx + 1, side + 1), 'v')
-        if (pos.p.y == 2 * side + 1 && pos.p.x in 3 * side + 1..4 * side)
-            return Pair(Point(3 * side, 2 * side - mx + 1), '<')
-        throw RuntimeException("CMH $pos $dir")
-    }
+    val (map1, _) = parse(readTestLines(2))
+    val (size1, layout1) = identify(map1)
+    check(size1, 4)
+    check(layout1, "BCFIJM")
+    checkWrapCube(Pos(Point(6, 1), '^'), size1, layout1, map1, Pos(Point(1, 14), '>'))
+    checkWrapCube(Pos(Point(12, 2), '>'), size1, layout1, map1, Pos(Point(8, 11), '<'))
+    checkWrapCube(Pos(Point(6, 12), 'v'), size1, layout1, map1, Pos(Point(4, 14), '<'))
+    checkWrapCube(Pos(Point(3, 9), '^'), size1, layout1, map1, Pos(Point(5, 7), '>'))
+    checkWrapCube(Pos(Point(8, 8), '>'), size1, layout1, map1, Pos(Point(12, 4), '^'))
+
+    val (map2, _) = parse(readLines())
+    val (size2, layout2) = identify(map2)
+    check(size2, 50)
+    check(layout2, "BCFIJM")
+
+    checkWrapCube(Pos(Point(150, 1), '>'), size2, layout2, map2, Pos(Point(100, 150), '<'))
+    checkWrapCube(Pos(Point(150, 10), '>'), size2, layout2, map2, Pos(Point(100, 141), '<'))
+    checkWrapCube(Pos(Point(100, 60), '>'), size2, layout2, map2, Pos(Point(110, 50), '^'))
+    checkWrapCube(Pos(Point(100, 110), '>'), size2, layout2, map2, Pos(Point(150, 41), '<'))
+    checkWrapCube(Pos(Point(50, 160), '>'), size2, layout2, map2, Pos(Point(60, 150), '^'))
+
+    checkWrapCube(Pos(Point(51, 10), '<'), size2, layout2, map2, Pos(Point(1, 141), '>'))
+    checkWrapCube(Pos(Point(51, 60), '<'), size2, layout2, map2, Pos(Point(10, 101), 'v'))
+    checkWrapCube(Pos(Point(1, 110), '<'), size2, layout2, map2, Pos(Point(51, 41), '>'))
+    checkWrapCube(Pos(Point(1, 160), '<'), size2, layout2, map2, Pos(Point(60, 1), 'v'))
+
+    checkWrapCube(Pos(Point(10, 101), '^'), size2, layout2, map2, Pos(Point(51, 60), '>'))
+    checkWrapCube(Pos(Point(60, 1), '^'), size2, layout2, map2, Pos(Point(1, 160), '>'))
+    checkWrapCube(Pos(Point(110, 1), '^'), size2, layout2, map2, Pos(Point(10, 200), '^'))
+
+    checkWrapCube(Pos(Point(10, 200), 'v'), size2, layout2, map2, Pos(Point(110, 1), 'v'))
+    checkWrapCube(Pos(Point(60, 150), 'v'), size2, layout2, map2, Pos(Point(50, 160), '<'))
+    checkWrapCube(Pos(Point(110, 50), 'v'), size2, layout2, map2, Pos(Point(100, 60), '<'))
 }
-throw RuntimeException("CMH")
 
- */
+private fun checkWrapCube(from: Pos, size: Int, layout: String, map: HashMap<Point, Char>, to: Pos) {
+
+    // val fromSide = side(from.p, size)
+    // val toSide = side(to.p, size)
+    // println("Wrap ${from.p} $fromSide${from.d} $toSide${to.d} ${to.p}")
+
+    val actual = wrapCube(from, size, layout)
+
+    if (actual != to)
+        throw RuntimeException("Failure: actual=$actual  expected=$to")
+
+    if (!map.containsKey(to.p))
+        throw RuntimeException("${to.p} not in map")
+}
