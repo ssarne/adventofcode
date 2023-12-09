f4d1b0f2198727883571e61964bb548c801c9492
ssarne <stefan.sarne@gmail.com>
2023-12-09 07:01:19 +0100

    2023 - dec 09 with check

--- src/main/kotlin/aoc/aoc2023/Dec09.kt
+++ src/main/kotlin/aoc/aoc2023/Dec09.kt
@@ -4,36 +4,36 @@ import aoc.ktutils.*
 
 fun main() {
     check(execute1(readTestLines(1)), 114)
-    execute1(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(1)) }
+    execute1(readLines()).let { println(it) ; check(it, readAnswerAsInt(1)) }
 
     check(execute2(readTestLines(1)), 2)
-    execute2(readLines()).let { println(it) } // ; check(it, readAnswerAsInt(2)) }
+    execute2(readLines()).let { println(it) ; check(it, readAnswerAsInt(2)) }
 }
 
-private fun execute1(input: List<String>): Long {
-    var sum = 0L
+private fun execute1(input: List<String>): Int {
+    var sum = 0
     for (line in input) {
-        val values = asLongArray(line)
+        val values = asIntArray(line)
         sum += diffAndPad(values).second
     }
     return sum
 }
 
-private fun execute2(input: List<String>): Long {
-    var sum = 0L
+private fun execute2(input: List<String>): Int {
+    var sum = 0
     for (line in input) {
-        val values = asLongArray(line)
+        val values = asIntArray(line)
         sum += diffAndPad(values).first
     }
     return sum
 }
 
-fun diffAndPad(values: LongArray): Pair<Long, Long> {
+fun diffAndPad(values: IntArray): Pair<Int, Int> {
 
     if (all(values, 0))
-        return 0L to 0L
+        return 0 to 0
 
-    val diff = LongArray(values.size - 1)
+    val diff = IntArray(values.size - 1)
     for (i in diff.indices)
         diff[i] = values[i + 1] - values[i]
 
