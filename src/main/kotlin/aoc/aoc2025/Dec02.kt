b219428172b5c4a21574a6233d83d68d3e785b20
ssarne <stefan.sarne@gmail.com>
2025-12-02 06:55:27 +0100

    2025 - dec 02 - minor cleanup

--- src/main/kotlin/aoc/aoc2025/Dec02.kt
+++ src/main/kotlin/aoc/aoc2025/Dec02.kt
@@ -1,32 +1,29 @@
 package aoc.aoc2025
 
 import aoc.ktutils.*
-import kotlin.math.*
 
 fun main() {
-    //execute1(readTestLines(1)).let { check(it, -1L) ; println("Test: $it") }
-    ///(readLines()).let { println(it) } // ; check(it, readAnswerAsLong(1)) }
+    execute1(readTestLines(1)).let { check(it, 1227775554L); println("Test: $it") }
+    execute1(readLines()).let { println(it); check(it, readAnswerAsLong(1)) }
 
-    execute2(readTestLines(1)).let { check(it, 4174379265L) ; println("Test: $it") }
-    execute2(readLines()).let { println(it) } // ; check(it, readAnswerAsLong(2)) }
+    execute2(readTestLines(1)).let { check(it, 4174379265L); println("Test: $it") }
+    execute2(readLines()).let { println(it); check(it, readAnswerAsLong(2)) }
 }
 
 private fun execute1(input: List<String>): Long {
 
     var sum = 0L
 
-    for (line in input) {
-        val ranges = line.split(",")
-        for (range in ranges) {
-            if (range == "") continue
-            val start = range.split("-").first().toLong()
-            val end = range.split("-").last().toLong()
-            for (i in start .. end) {
-                val id = i.toString()
-                val token = id.substring(0, id.length / 2)
-                val tt = token + token
-                if (tt == id) sum += i
-            }
+    val ranges = input.first().split(",")
+    for (range in ranges) {
+        val start = range.split("-").first().toLong()
+        val end = range.split("-").last().toLong()
+        for (i in start..end) {
+            val id = i.toString()
+            val t = id.substring(0, id.length / 2)
+            val tt = t + t
+            if (tt == id)
+                sum += i
         }
     }
 
@@ -34,26 +31,23 @@ private fun execute1(input: List<String>): Long {
 }
 
 private fun execute2(input: List<String>): Long {
+
     var sum = 0L
 
-    for (line in input) {
-        val ranges = line.split(",")
-        for (range in ranges) {
-            if (range == "") continue
-            val start = range.split("-").first().toLong()
-            val end = range.split("-").last().toLong()
-            for (i in start .. end) {
-                val id = i.toString()
-                for (j in 1 .. id.length - 1) {
-                    val token = id.substring(0, j)
-                    var tt = token + token
-                    while (tt.length < id.length)
-                        tt += token
-                    if (tt == id) {
-                        sum += i
-                        println(i)
-                        break
-                    }
+    val ranges = input.first().split(",")
+    for (range in ranges) {
+        val start = range.split("-").first().toLong()
+        val end = range.split("-").last().toLong()
+        for (i in start..end) {
+            val id = i.toString()
+            for (j in 1..id.length - 1) {
+                val t = id.substring(0, j)
+                var tt = t + t
+                while (tt.length < id.length)
+                    tt += t
+                if (tt == id) {
+                    sum += i
+                    break
                 }
             }
         }
