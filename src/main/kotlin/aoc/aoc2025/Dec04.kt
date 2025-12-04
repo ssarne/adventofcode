5778eee9a9c94bcf87bdc1621586b3435b84a741
ssarne <stefan.sarne@gmail.com>
2025-12-04 06:33:50 +0100

    2025 - dec 04 - simplified

--- src/main/kotlin/aoc/aoc2025/Dec04.kt
+++ src/main/kotlin/aoc/aoc2025/Dec04.kt
@@ -1,33 +1,26 @@
 package aoc.aoc2025
 
-
 import aoc.ktutils.*
 import kotlin.math.*
 
 fun main() {
-    execute1(readTestLines(1)).let { check(it, -1L); println("Test: $it") }
-    execute1(readLines()).let { println(it) } // ; check(it, readAnswerAsLong(1)) }
+    execute1(readTestLines(1)).let { check(it, 13L); println("Test: $it") }
+    execute1(readLines()).let { println(it); check(it, readAnswerAsLong(1)) }
 
-    execute2(readTestLines(1)).let { check(it, -1L) ; println("Test: $it") }
-    execute2(readLines()).let { println(it) } // ; check(it, readAnswerAsLong(2)) }
+    execute2(readTestLines(1)).let { check(it, 43L); println("Test: $it") }
+    execute2(readLines()).let { println(it); check(it, readAnswerAsLong(2)) }
 }
 
 private fun execute1(input: List<String>): Long {
     val grid = parseCharacterGridToMap(input)
-    val size = mapSize(grid)
     var count = 0L
-    for (x in size.first.x..size.second.x)
-        for (y in size.first.y..size.second.y) {
-            if (grid[Point(x, y)] == '@') {
-                val around = Point(x, y).surrounding()
-                var n = 0
-                for (p in around)
-                    if (grid.get(p) == '@')
-                        n++
-                if (n < 4)
-                    count++
-            }
+
+    for (p in grid) {
+        if (p.value == '@') {
+            val n = p.key.surrounding().count { grid[it] == '@' }
+            if (n < 4) count++
         }
+    }
     return count
 }
 
@@ -35,27 +28,23 @@ private fun execute2(input: List<String>): Long {
     val grid = parseCharacterGridToMap(input)
     val size = mapSize(grid)
     var count = 0L
-    var changed = true
-    while (changed) {
 
-        changed = false
-
-        for (x in size.first.x..size.second.x)
+    do {
+        var changed = false
+        for (x in size.first.x..size.second.x) {
             for (y in size.first.y..size.second.y) {
-                if (grid[Point(x, y)] == '@') {
-                    val around = Point(x, y).surrounding()
-                    var n = 0
-                    for (p in around)
-                        if (grid.get(p) == '@')
-                            n++
+                val p = Point(x, y)
+                if (grid[p] == '@') {
+                    val n = p.surrounding().count { grid[it] == '@' }
                     if (n < 4) {
-                        grid.remove(Point(x, y))
+                        grid.remove(p)
                         count++
                         changed = true
-                        println("Remove " + Point(x, y))
                     }
                 }
             }
-    }
+        }
+    } while (changed)
+
     return count
 }
