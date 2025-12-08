bb6dc191c7b18cc90fa9577cf3025f979b48a337
ssarne <stefan.sarne@gmail.com>
2025-12-08 21:55:40 +0100

    2025 - dec 08 - unified, faster

--- src/main/kotlin/aoc/aoc2025/Dec08.kt
+++ src/main/kotlin/aoc/aoc2025/Dec08.kt
@@ -3,122 +3,67 @@ package aoc.aoc2025
 import aoc.ktutils.*
 
 fun main() {
+    val test = execute(readTestLines(1), 10)
+    val result = execute(readLines(), 1000)
 
-    execute1(readTestLines(1), 10).let { check(it, 40L) ; println("Test: $it") }
-    execute1(readLines(), 1000).let { println(it) ; check(it, readAnswerAsLong(1)) }
+    check(test.first, 40L)
+    check(result.first, readAnswerAsLong(1))
 
-    execute2(readTestLines(1)).let { check(it, 25272L) ; println("Test: $it") }
-    execute2(readLines()).let { println(it) ; check(it, readAnswerAsLong(2)) }
+    check(test.second, 25272L)
+    check(result.second, readAnswerAsLong(2))
+
+    println("Test: $test")
+    println("Result: $result")
 }
 
-private fun execute1(input: List<String>, connections: Int): Long {
+private fun execute(input: List<String>, steps: Int): Pair<Long, Long> {
 
     val boxes = ArrayList<Point3D>()
-    val circuits = HashMap<String, HashSet<Point3D>>()
-    val boxToC = HashMap<Point3D, String>() // box -> circuit
-    val pairs = HashSet<String>() // box -> circuit
+    val boxToC = HashMap<Point3D, String>() // box -> circuit label
+    val circuits = HashMap<String, Int>()
+    val connections = ArrayList<Triple<Point3D, Point3D, Double>>()   // box1, box2, distance
+    var part1 = 0L
+    var part2 = 0L
 
     for (line in input) {
         val split = line.split(",")
         val point = Point3D(split[0].toLong(), split[1].toLong(), split[2].toLong())
         val circuitId = point.toString()
-        val circuit = HashSet<Point3D>()
         boxes.add(point)
-        circuit.add(point)
         boxToC.put(point, circuitId)
-        circuits.put(circuitId, circuit)
+        circuits.put(circuitId, 1)
     }
 
-    repeat(connections) {
-        var from = Point3D(0, 0, 0)
-        var to = Point3D(0, 0, 0)
-        var dist = Double.MAX_VALUE
-        for (p1 in boxes) {
-            for (p2 in boxes) {
-                if (p1 == p2) continue
-                if (pairs.contains("$p1 $p2")) continue
-                if (dist > p1.euclideanDistance(p2)) {
-                    from = p1
-                    to = p2
-                    dist = p1.euclideanDistance(p2)
-                }
-            }
-        }
+    for (i in 0 until boxes.size - 1)
+        for (j in i + 1 until boxes.size)
+            connections.add(Triple(boxes[i], boxes[j], boxes[i].euclideanDistance(boxes[j])))
 
-        pairs.add("$from $to")
-        pairs.add("$to $from")
+    connections.sortWith(compareBy({it.third}))
 
-        if (boxToC[from] != boxToC[to]) {
-            val toCircuitId = boxToC[to]!!
-            val toCircuit = circuits[toCircuitId]!!
-            val fromCircuitId = boxToC[from]!!
-            val fromCircuit = circuits[fromCircuitId]!!
-            toCircuit.addAll(fromCircuit)
+    for (i in generateSequence(0) { it + 1 }.takeWhile { circuits.size > 1 }) {
+
+        val connection = connections[i]
+
+        if (boxToC[connection.first] != boxToC[connection.second]) {
+            val toCircuitId = boxToC[connection.first]!!
+            val fromCircuitId = boxToC[connection.second]!!
+            for (e in boxToC)
+                if (e.value == fromCircuitId)
+                    boxToC[e.key] = toCircuitId
+
+            circuits[toCircuitId] = circuits[toCircuitId]!! + circuits[fromCircuitId]!!
             circuits.remove(fromCircuitId)
-            for (p in fromCircuit) boxToC[p] = toCircuitId
         }
 
-        // println("Connecting $from $to  ${circuits.size}")
-        if (circuits.size == 1) return from.x * to.x
-    }
-
-    val cs = circuits.values.map { it -> it.size }.toMutableList().sortedDescending()
-
-    return 1L * cs[0] * cs[1] * cs[2]
-}
-
-private fun execute2(input: List<String>): Long {
-
-    val boxes = ArrayList<Point3D>()
-    val circuits = HashMap<String, HashSet<Point3D>>()
-    val boxToC = HashMap<Point3D, String>() // box -> circuit
-    val pairs = HashSet<String>() // box -> circuit
-
-    for (line in input) {
-        val split = line.split(",")
-        val point = Point3D(split[0].toLong(), split[1].toLong(), split[2].toLong())
-        val circuitId = point.toString()
-        val circuit = HashSet<Point3D>()
-        boxes.add(point)
-        circuit.add(point)
-        boxToC.put(point, circuitId)
-        circuits.put(circuitId, circuit)
-    }
-
-    while (circuits.size > 1) {
-        var from = Point3D(0, 0, 0)
-        var to = Point3D(0, 0, 0)
-        var dist = Double.MAX_VALUE
-        for (p1 in boxes) {
-            for (p2 in boxes) {
-                if (p1 == p2) continue
-                if (pairs.contains("$p1 $p2")) continue
-                if (dist > p1.euclideanDistance(p2)) {
-                    from = p1
-                    to = p2
-                    dist = p1.euclideanDistance(p2)
-                }
-            }
+        if (i+1 == steps) {
+            val cs = circuits.values.toMutableList().sortedDescending()
+            part1 = 1L * cs[0] * cs[1] * cs[2]
         }
 
-        pairs.add("$from $to")
-        pairs.add("$to $from")
-
-        if (boxToC[from] != boxToC[to]) {
-            val toCircuitId = boxToC[to]!!
-            val toCircuit = circuits[toCircuitId]!!
-            val fromCircuitId = boxToC[from]!!
-            val fromCircuit = circuits[fromCircuitId]!!
-            toCircuit.addAll(fromCircuit)
-            circuits.remove(fromCircuitId)
-            for (p in fromCircuit) boxToC[p] = toCircuitId
+        if (circuits.size == 1) {
+            part2 = connections[i].first.x * connections[i].second.x
         }
-
-        // println("Connecting $from $to  ${circuits.size}")
-        if (circuits.size == 1) return from.x * to.x
     }
 
-    val cs = circuits.values.map { it -> it.size }.toMutableList().sortedDescending()
-
-    return 1L * cs[0] * cs[1] * cs[2]
+    return part1 to part2
 }
