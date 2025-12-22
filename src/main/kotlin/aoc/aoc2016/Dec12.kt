726fb577c8ce6f4c342aa2a181370f7c17860ed8
ssarne <stefan.sarne@gmail.com>
2025-12-22 22:49:30 +0100

    2016 - dec 12

---
+++ src/main/kotlin/aoc/aoc2016/Dec12.kt
@@ -0,0 +1,55 @@
+package aoc.aoc2016
+
+import aoc.ktutils.*
+
+fun main() {
+    execute1(readTestLines(1)).let { check(it, 42L) ; println("Test: $it") }
+    execute1(readLines()).let { println(it) ; check(it, readAnswerAsLong(1)) }
+
+    execute2(readTestLines(1)).let { check(it, 42L) ; println("Test: $it") }
+    execute2(readLines()).let { println(it) ; check(it, readAnswerAsLong(2)) }
+}
+
+private fun execute1(input: List<String>): Long {
+    val regs = mutableMapOf('a' to 0L, 'b' to 0L, 'c' to 0L, 'd' to 0L)
+    return execute(input, regs)
+}
+
+private fun execute2(input: List<String>): Long {
+    val regs = mutableMapOf('a' to 0L, 'b' to 0L, 'c' to 1L, 'd' to 0L)
+    return execute(input, regs)
+}
+
+private fun regOrVal(str: String, regs: MutableMap<Char, Long>): Long {
+    if ("abcd".contains(str.first()))
+        return regs[str.first()]!!
+    return str.toLong()
+}
+
+private fun execute(input: List<String>, regs: MutableMap<Char, Long>): Long {
+
+    var ip = 0
+    do {
+        val instr = input[ip++].split(" ")
+        when(instr[0]) {
+            "cpy" -> {
+                val v = regOrVal(instr[1], regs)
+                regs[instr[2].first()] = v
+            }
+            "inc" -> {
+                val reg = instr[1].first()
+                regs[reg] = regs[reg]!!.inc()
+            }
+            "dec" -> {
+                val reg = instr[1].first()
+                regs[reg] = regs[reg]!!.dec()
+            }
+            "jnz" -> {
+                val v = regOrVal(instr[1], regs)
+                if (v != 0L)
+                    ip = ip + instr[2].toInt() - 1 // already incremented one at read
+            }
+        }
+    } while(ip < input.size)
+    return regs['a']!!.toLong()
+}
